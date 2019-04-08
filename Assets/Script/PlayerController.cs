f556dbbb170d6ab455e4e688448fa6fac57ed0ef
puthusseri <vyshakputhusseri@gmail.com>
2019-04-08 19:18:31 +0530

    The networktransform was added so that the player can identify its enemy by color, i had given the white color for the enemy'

--- Assets/Script/PlayerController.cs
+++ Assets/Script/PlayerController.cs
@@ -23,5 +23,9 @@ public class PlayerController : NetworkBehaviour
 		transform.Rotate(0,x,0);
 		transform.Translate(0,0,z);
 	}
+	public override void OnStartLocalPlayer()
+	{
+		GetComponent<MeshRenderer>().material.color = Color.blue;
+	}
 	
 }

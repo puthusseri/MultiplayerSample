6ab5f4995dbe870adc3b344508e8de3f1200a11c
puthusseri <vyshakputhusseri@gmail.com>
2019-04-08 21:19:17 +0530

    Health bar was synched

--- Assets/Script/Health.cs
+++ Assets/Script/Health.cs
@@ -2,22 +2,33 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+usimg UnityEngine.Networking;
 
 
-public class Health : MonoBehaviour
+public class Health : NetworkBehaviour
 {
     public const int maxHealth  = 100;
-	public int currentHealth = maxHealth;
+	[SyncVar(hook = "OnChangeHealth")]public int currentHealth = maxHealth;
 	public RectTransform healthbar;
 	
 	public void TakeDamage(int amount)
 	{
+		if(!isServer)
+		{
+			return;
+		}
 		currentHealth -= amount;
 		if(currentHealth<=0)
 		{
 			currentHealth = 0;
 			Debug.Log("Dead");
 		}
-		healthbar.sizeDelta = new Vector2(currentHealth*2,healthbar.sizeDelta.y);
 	}
+	void OnChangeHealth(int health) {
+		
+		healthbar.sizeDelta = new Vector2(health*2,healthbar.sizeDelta.y);
+	
+	}
+		 
+	
 }
